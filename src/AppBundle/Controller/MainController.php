66e758fdd045a88a5ed062f5e1260b222d2e857a
Unknown <sofyann.fahim@gmail.com>
2017-12-15 17:28:09 +0100

    Création homepage + mise en place de la bannière

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -1,4 +1,10 @@
 <?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 /**
  * Created by PhpStorm.
  * User: Sofyann
@@ -6,7 +12,12 @@
  * Time: 15:29
  */
 
-class MainController
+class MainController extends Controller
 {
+
+    public function homeAction(){
+
+        return $this->render('home.html.twig');
+    }
 
 }
